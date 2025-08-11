4d09bc213e12100d69fe91bba1048e9a91dd1ac8
Axel Eldrian <a.hadiwibowo@moovd.nl>
2025-08-11 14:12:51 +0700

    feat: complete login by google

--- src/pages/AuthScreen.tsx
+++ src/pages/AuthScreen.tsx
@@ -1,7 +1,33 @@
 import AuthLayout from "../components/layouts/AuthLayout";
 import GoogleAuthButton from "../components/buttons/GoogleAuthButton";
+import { useLoginWithOAuth } from "@privy-io/react-auth";
+import { useNavigate } from "react-router-dom";
 
 const AuthScreen = () => {
+  const navigate = useNavigate();
+  
+  const { initOAuth } = useLoginWithOAuth({
+    onComplete: ({ user, isNewUser }) => {
+      console.log("User logged in successfully", user);
+      if (isNewUser) {
+        // Perform actions for new users
+      }
+
+      navigate("/libraries");
+    },
+    onError: (error) => {
+      console.error("Login failed", error);
+    },
+  });
+
+  const handleGoogleSignIn = async () => {
+    try {
+      await initOAuth({ provider: "google" });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <AuthLayout>
       <div className="flex flex-col justify-center items-center">
@@ -13,7 +39,7 @@ const AuthScreen = () => {
 
       <div className="w-full mt-4 flex-col items-start">
         <div className="flex flex-col items-center">
-          <GoogleAuthButton />
+          <GoogleAuthButton onClick={handleGoogleSignIn} />
 
           <div className="mx-auto max-w-xs">
             <p className="mt-6 text-xs text-gray-600 text-center">
