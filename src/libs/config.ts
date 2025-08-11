8f10e712997bcf6cfbab5542e9983f40ae558864
Axel Eldrian <a.hadiwibowo@moovd.nl>
2025-08-11 13:21:48 +0700

    setup env

---
+++ src/libs/config.ts
@@ -0,0 +1,10 @@
+const config = {
+  env: {
+    privy: {
+      appId: import.meta.env.VITE_PRIVY_APP_ID,
+      clientId: import.meta.env.VITE_PRIVY_CLIENT_ID,
+    },
+  },
+};
+
+export default config;
--- src/providers/PrivyProvider.tsx
+++ src/providers/PrivyProvider.tsx
@@ -1,14 +1,12 @@
 import { PrivyProvider } from "@privy-io/react-auth";
+import config from "../libs/config";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  const privyAppId = process.env.PRIVY_APP_ID!;
-  const privyClientId = process.env.PRIVY_CLIENT_ID!;
   return (
     <PrivyProvider
-      appId={privyAppId}
-      clientId={privyClientId}
+      appId={config.env.privy.appId}
+      clientId={config.env.privy.clientId}
       config={{
-        // Create embedded wallets for users who don't have a wallet
         embeddedWallets: {
           ethereum: {
             createOnLogin: "users-without-wallets",
