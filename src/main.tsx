cfd7ba1554223ca55103cc1a4b9b95f35faa5175
Axel Eldrian <a.hadiwibowo@moovd.nl>
2025-08-11 14:15:32 +0700

    fix: redirect routing issue

--- src/main.tsx
+++ src/main.tsx
@@ -5,21 +5,20 @@ import Providers from "./providers/PrivyProvider.tsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import AuthScreen from "./pages/AuthScreen.tsx";
 import LibraryScreen from "./pages/LibraryScreen.tsx";
-import ProtectedRoute from "./routes/ProtectedRoute.tsx";
 
 const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AuthScreen />,
+  },
   {
     path: "/auth",
     element: <AuthScreen />,
   },
   {
-    element: <ProtectedRoute />,
-    children: [
-      { path: "/", element: <LibraryScreen /> },
-      { path: "/libraries", element: <LibraryScreen /> }
-    ]
-  }
-  
+    path: "/libraries",
+    element: <LibraryScreen />,
+  },
 ]);
 
 createRoot(document.getElementById("root")!).render(
