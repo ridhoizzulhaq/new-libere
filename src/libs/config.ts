3041b1c823e628599315166b94fc1c8dc31a2975
Axel Eldrian <a.hadiwibowo@moovd.nl>
2025-08-11 20:38:39 +0700

    feat: update config

--- src/libs/config.ts
+++ src/libs/config.ts
@@ -4,6 +4,10 @@ const config = {
       appId: import.meta.env.VITE_PRIVY_APP_ID,
       clientId: import.meta.env.VITE_PRIVY_CLIENT_ID,
     },
+    pinata: {
+      apiKey: import.meta.env.VITE_PINATA_API_KEY,
+      secretApiKey: import.meta.env.VITE_PINATA_SECRET_API_KEY,
+    },
   },
 };
 
