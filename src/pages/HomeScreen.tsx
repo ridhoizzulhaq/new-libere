2378cb5a23fe3f9661a3bc254a11ff5c7921c291
Axel Eldrian <a.hadiwibowo@moovd.nl>
2025-08-13 07:58:01 +0700

    feat: complete header

--- src/pages/HomeScreen.tsx
+++ src/pages/HomeScreen.tsx
@@ -6,9 +6,22 @@ const HomeScreen = () => {
   const latestBooks = DummyBooks;
   return (
     <HomeLayout>
-      <BookList
-        books={latestBooks.slice(1)}
-      />
+      <div className="w-full h-full flex flex-col items-center justify-center">
+        <section className="w-full h-full flex flex-col items-start justify-start max-w-[90vw] px-[4rem] py-[2rem] gap-[1.875rem]">
+          <div className="flex flex-col items-center justify-center gap-[1rem] w-full text-center">
+            <h1 className="w-[75%] max-w-[50rem] font-bold  text-5xl capitalize text-dark-900">
+              BookChain Revolution - Your Books, Your Rules, Your Rewards"
+            </h1>
+            <p className="w-[60%] text-lg text-center">
+              Experience the future of digital libraries where readers control
+              the ecosystem. Borrow, lend, and discover books while earning
+              rewards through our innovative blockchain system. Every page
+              turned, every book shared - transparently recorded forever.
+            </p>
+          </div>
+        </section>
+      </div>
+      <BookList books={latestBooks.slice(1)} />
     </HomeLayout>
   );
 };
